e73d810ba6653c30ec1fd774fd3928df118e333b
Toni Ylenius <toni.ylenius@iki.fi>
2015-05-23 17:41:37 +0300

    new command, zocker diff

---
+++ cmds/diff.sh
@@ -0,0 +1,30 @@
+if [ -n "$INFO" ]
+then
+	echo "Show changes in a container"
+	exit 0
+fi
+set -u
+set -e
+
+help() {
+	echo "usage: diff [-h] <container>"
+}
+
+## Main
+
+. "$LIB/lib.sh"
+load_configs
+check_zfs_dirs
+
+check_getopts_help $@
+
+if [ $# -ne 1 ]
+then
+	help
+	exit 1
+fi
+jail="$1"
+jail_dir=`get_zfs_path "$ZFS_FS/jails/$jail"`
+imageid="`cat $jail_dir/imageid`"
+
+zfs diff "$ZFS_FS/images/$imageid/z"@clean "$ZFS_FS/jails/$jail"/z
