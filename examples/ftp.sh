17b140d55f7b1e9679e04860d6c7f8298f33a6e5
Toni Ylenius <toni.ylenius@iki.fi>
2015-08-25 23:04:44 +0300

    An example to create a base image using FTP

---
+++ examples/ftp.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+set -u
+
+FREEBSD_HOST=${FREEBSD_HOST-http://ftp.freebsd.org/pub/FreeBSD/releases/}
+ARCH=${ARCH-amd64}
+RELEASE=${RELEASE-10.2-RELEASE}
+zocker create -n ftpbuild scratch
+
+jail_path=`zocker inspect ftpbuild | awk -F : '/^path/ {print $2}'`
+for dist in base.txz lib32.txz
+do
+	echo "# $dist"
+	fetch "$FREEBSD_HOST/$ARCH/$RELEASE/$dist"
+	tar -xpf "$dist" -C "${jail_path}/z/"
+	rm "$dist"
+done
+
+zocker commit ftpbuild "$RELEASE"
+zocker rm ftpbuild
+zocker run -n build "$RELEASE" "freebsd-update --not-running-from-cron fetch install; rm -rf /var/db/freebsd-update/files"
+zocker commit build "$RELEASE"
+zocker rm build
